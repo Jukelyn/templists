d2ca743d6c4082e7ab746d6446ac0d992841916f
Jukelyn <viperkillergaming@gmail.com>
2025-04-23 11:48:09 -0400

    Changed how itemIds are generated

--- components/TemplistCard.tsx
+++ components/TemplistCard.tsx
@@ -12,6 +12,35 @@ interface TemplistCardProps {
   onSave: (updatedItems: TemplistItem[]) => void;
 }
 
+const getItemId = (
+  templistId: number,
+  initialItems: TemplistItem[],
+): string => {
+  const currentSavedMaxItemId = getLastItemIdForTemplist(templistId);
+
+  if (initialItems && initialItems.length > 0) {
+    const initialMaxItemId = Math.max(
+      ...initialItems.map((t) => parseInt(t.itemId, 10) + 1),
+    );
+
+    return Math.max(currentSavedMaxItemId, initialMaxItemId).toString();
+  }
+
+  return currentSavedMaxItemId.toString();
+};
+
+const getLastItemIdForTemplist = (templistId: number): number => {
+  const templist = data.templists.find(
+    (list) => list.templistId === templistId,
+  );
+
+  if (!templist || !templist.items || templist.items.length === 0) {
+    return 0;
+  }
+
+  return Math.max(...templist.items.map((t) => parseInt(t.itemId, 10) + 1));
+};
+
 export const TemplistCard: React.FC<TemplistCardProps> = ({
   templistId,
   items: initialItems,
