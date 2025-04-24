8f23832e81e3ae0fb19f5708573d0f44e156989b
Jukelyn <viperkillergaming@gmail.com>
2025-04-24 00:53:57 -0400

    Moved reducer to another file

--- app/page.tsx
+++ app/page.tsx
@@ -6,61 +6,7 @@ import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Templist, TemplistItem } from "@/types/templist";
 import { toast } from "sonner";
-import { ActionTypes } from "@/types/actions";
-
-function reducer(state: Templist[], action: ActionTypes): Templist[] {
-  switch (action.type) {
-    case "SET_INITIAL_STATE":
-      return [...action.templists].sort((a, b) => a.templistId - b.templistId);
-    case "UPDATE_ITEMS":
-      return state.map((t) =>
-        t.templistId === action.templistId
-          ? { ...t, items: action.newItems }
-          : t,
-      );
-    case "ADD_TEMPLIST":
-      if (state.some((t) => t.templistId === action.newTemplist.templistId)) {
-        console.warn(
-          `Templist with ID ${action.newTemplist.templistId} already exists.`,
-        );
-        return state;
-      }
-
-      return [...state, action.newTemplist];
-    case "REMOVE_TEMPLIST":
-      const templistToRemove = state.find(
-        (t) => t.templistId === action.templistId,
-      );
-      if (!templistToRemove) {
-        console.warn(
-          `Templist with ID ${action.templistId} not found for removal.`,
-        );
-
-        throw Error(`Templist of id ${action.templistId} not found.`);
-      }
-      const currentSavedLists = localStorage.getItem("Templists");
-      if (currentSavedLists) {
-        try {
-          const parsedData = JSON.parse(currentSavedLists) as {
-            templists: Templist[];
-          };
-          const updatedTemplists = parsedData.templists.filter(
-            (t) => t.templistId !== action.templistId,
-          );
-          localStorage.setItem(
-            "Templists",
-            JSON.stringify({ templists: updatedTemplists }),
-          );
-        } catch (error) {
-          console.error("Error parsing localStorage data:", error);
-        }
-      }
-
-      return state.filter((t) => t.templistId !== action.templistId);
-    default:
-      throw Error("Unknown action.");
-  }
-}
+import { TemplistReducer } from "@/lib/utils/TemplistReducer";
 
 const getLastTemplistId = (templists: Templist[]): number => {
   if (!templists || templists.length === 0) {
@@ -70,7 +16,7 @@ const getLastTemplistId = (templists: Templist[]): number => {
 };
 
 export default function ChecklistApp() {
-  const [templistCards, dispatch] = useReducer(reducer, []);
+  const [templistCards, dispatch] = useReducer(TemplistReducer, []);
   const isDataLoaded = useRef(false);
   const nextId = useRef(0);
 
