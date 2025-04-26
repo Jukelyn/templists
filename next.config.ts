60441789ba1f19c90f5b924bad448c175f76533b
Jukelyn <viperkillergaming@gmail.com>
2025-04-26 04:45:52 -0400

    Added millionlint and changed --turbo to turbopack in next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
+import MillionLint from "@million/lint";
 
 const nextConfig: NextConfig = {
   // experimental: {
   //   reactCompiler: true,
   // },
+  turbopack: {},
   devIndicators: false,
   output: "standalone",
 };
 
-export default nextConfig;
+export default MillionLint.next({ rsc: true })(nextConfig);
