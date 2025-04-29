e4c55d85bcfe7b56f82121b8071c4c4cad4dff0f
Jukelyn <viperkillergaming@gmail.com>
2025-04-29 16:18:36 -0400

    bro million lint makes my sht soooo slow to compile

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
-import MillionLint from "@million/lint";
 
 const nextConfig: NextConfig = {
-  // experimental: {
-  //   reactCompiler: true,
-  // },
+  experimental: {
+    reactCompiler: true,
+  },
   turbopack: {},
   devIndicators: false,
   output: "standalone",
 };
 
-export default MillionLint.next({ rsc: true })(nextConfig);
+export default nextConfig;
