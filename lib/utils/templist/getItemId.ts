295356d69c7f577eef7e6ab84bc57e8b6b876f49
Jukelyn <viperkillergaming@gmail.com>
2025-04-30 14:46:33 -0400

    Fixed possible undefined itemId

--- lib/utils/templist/getItemId.ts
+++ lib/utils/templist/getItemId.ts
@@ -4,9 +4,7 @@ const getLastItemIdForTemplist = (ulid: string): number => {
   const lists = JSON.parse(localStorage.getItem("Templists") || "{}") as {
     templists: Templist[];
   };
-  const templist = lists.templists.find(
-    (list) => list.ulid === ulid,
-  );
+  const templist = lists.templists?.find((list) => list.ulid === ulid);
 
   if (!templist || !templist.items || templist.items.length === 0) {
     return 0;
@@ -15,10 +13,7 @@ const getLastItemIdForTemplist = (ulid: string): number => {
   return Math.max(...templist.items.map((t) => parseInt(t.itemId, 10) + 1));
 };
 
-const getItemId = (
-  ulid: string,
-  initialItems: TemplistItem[],
-): string => {
+const getItemId = (ulid: string, initialItems: TemplistItem[]): string => {
   const currentSavedMaxItemId = getLastItemIdForTemplist(ulid);
 
   if (initialItems && initialItems.length > 0) {
