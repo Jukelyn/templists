b6c93cc607266c9b00cccc5f458d144479314353
Jukelyn <viperkillergaming@gmail.com>
2025-05-01 00:24:15 -0400

    Use ToggleGroup instead of implementing my own

--- components/ui/sidebar/sidebar-toolbar.tsx
+++ components/ui/sidebar/sidebar-toolbar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { LayoutPanelLeft, Rows2, Grid2x2 } from "lucide-react";
 import {
   Tooltip,
@@ -9,6 +8,7 @@ import {
 } from "@/components/ui/sidebar/tooltip";
 import { SidebarTrigger } from "@/components/ui/sidebar/sidebar";
 import { CommandShortcut } from "@/components/ui/search-bar/command";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 import { useTemplistContext } from "@/components/ui/templist/TemplistContext";
 import { LayoutType } from "@/components/ui/templist/TemplistContext";
@@ -35,49 +35,44 @@ function SidebarTriggerWithToolTip() {
 export default function SidebarToolbar({
   className,
 }: React.HTMLAttributes<HTMLDivElement>) {
-  const { templistCards, changeLayout, layout } = useTemplistContext();
+  const { templistCards, changeLayout } = useTemplistContext();
 
   function handleClick(layout: LayoutType) {
     changeLayout(layout);
   }
 
-  const activeButtonStyle = "text-card bg-gray-300";
-
   return (
     <div className={cn("mt-4 flex items-center justify-end", className)}>
       <div className="hidden md:block">
-        <div
-          className="mr-2 flex flex-row rounded-lg border-2 border-current"
+        <ToggleGroup
+          type="single"
           aria-label="Layout Selector"
+          className="z-20 mr-2 rounded-lg border-2 border-current"
         >
-          <Button
-            variant="ghost"
-            className={cn(layout === "list" && activeButtonStyle)}
+          <ToggleGroupItem
+            value="list"
             aria-label="List Layout"
             onClick={() => handleClick("list")}
           >
             <Rows2 className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="ghost"
-            className={cn(layout === "grid" && activeButtonStyle)}
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="grid"
             aria-label="Grid Layout"
             onClick={() => handleClick("grid")}
           >
             <Grid2x2 className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="ghost"
-            className={cn(
-              templistCards.length > 3 ? "hidden lg:block" : "hidden",
-              layout === "masonry" && activeButtonStyle,
-            )}
-            aria-label="Masonry Layout"
-            onClick={() => handleClick("masonry")}
-          >
-            <LayoutPanelLeft className="h-4 w-4" />
-          </Button>
-        </div>
+          </ToggleGroupItem>
+          {templistCards.length > 3 && (
+            <ToggleGroupItem
+              value="masonry"
+              aria-label="Masonry Layout"
+              onClick={() => handleClick("masonry")}
+            >
+              <LayoutPanelLeft className="h-4 w-4" />
+            </ToggleGroupItem>
+          )}
+        </ToggleGroup>
       </div>
       <SidebarTriggerWithToolTip />
     </div>
